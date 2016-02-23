0826f03f22a0e8b57e0d5426978ae29dc882fcfc
David Pikas <david@pikas.se>
2016-02-23 16:15:35 +0100

    maps image to 2DArray of gray values

--- test.hs
+++ test.hs
@@ -1,15 +1,25 @@
 import Codec.Picture
-
-
+import Codec.Picture.Types
 
 main :: IO ()
 main = do
     fp <- getLine
     image <- readImage fp
-    case image of 
-        Right image' -> print . grey . middlePixel $ image'
+    case image of
+        Right image' -> print . pixelMatrix . toImage8 $ image'
         _            -> print "Error"
 
-middlePixel (ImageRGB8 image@(Image w h _)) = pixelAt image (w `div` 2) (h `div` 2)
+toImage8 :: DynamicImage -> Image Pixel8
+toImage8 (ImageRGB8  img) = pixelMap (\(PixelRGB8  r g b) -> (fromIntegral $ r + g + b) `div` 3      ) img
+toImage8 (ImageRGB16 img) = pixelMap (\(PixelRGB16 r g b) -> (fromIntegral $ r + g + b) `div` (3*256)) img
+-- etc.
 
-grey (PixelRGB8 r g b) = (r + g + b) `div` 3
+
+pixelMatrix ::  Image Pixel8 -> [[Int]]
+pixelMatrix =
+	snd . pixelFold
+		(\(lastY, ps:pss) x y p ->
+			if y == lastY
+				then (y, (fromIntegral p:ps):pss)
+				else (y, [fromIntegral p]:ps:pss))
+		(0,[[]])
